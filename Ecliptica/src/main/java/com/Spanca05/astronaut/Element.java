90c03e003ff90086494e4608d5b4adfab26cf405
Vasco Vieira <up202307797@up.pt>
2024-11-25 11:03:51 +0000

    Create Class Position and Class Element Classes will extend Class Element

---
+++ Ecliptica/src/main/java/com/Spanca05/astronaut/Element.java
@@ -0,0 +1,21 @@
+package com.Spanca05.astronaut;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+
+public abstract class Element {
+    private Position position;
+
+    public Element(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public abstract void draw(TextGraphics graphics);
+}
---
+++ Ecliptica/src/main/java/com/Spanca05/astronaut/Position.java
@@ -0,0 +1,40 @@
+package com.Spanca05.astronaut;
+
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Position(Position position) {
+        this.x = position.getX();
+        this.y = position.getY();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean equals(Position position) {
+        if(this.x == position.getX() && this.y == position.getY()) {
+            return true;
+        }
+        return false;
+    }
+
+}
