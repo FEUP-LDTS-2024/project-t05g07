d35976f4b6df3915e95b242ce66129f75e7f445a
Vasco Vieira <up202307797@up.pt>
2024-11-25 11:10:33 +0000

    Add Class Monster Class Monster extends Class Element

---
+++ Ecliptica/src/main/java/com/Spanca05/astronaut/Monster.java
@@ -0,0 +1,42 @@
+package com.Spanca05.astronaut;
+
+import com.googlecode.lanterna.SGR;
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyType;
+
+import java.util.Random;
+
+public class Monster extends Element {
+
+    public Monster(Position position) {
+        super(position);
+    }
+
+    public Monster(int x, int y) {
+        this(new Position(x, y));
+    }
+
+    @Override
+    public void draw(TextGraphics graphics) {
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.setForegroundColor(TextColor.Factory.fromString("#669900"));
+        graphics.enableModifiers(SGR.BOLD);
+        graphics.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "M");
+    }
+
+    public Position move(Position pos) {
+        return randomMove(pos);
+    }
+
+    public Position randomMove(Position position) {
+        int max = 1;
+        int min = -1;
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            return new Position(position.getX() + (random.nextBoolean() ? min : max), position.getY());
+        }
+        return new Position(position.getX(), position.getY() + (random.nextBoolean() ? min : max));
+    }
+}
