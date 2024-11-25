2f63d99ff74564efa64dbf8fe358f6db1796068f
Vasco Vieira <up202307797@up.pt>
2024-11-25 10:52:48 +0000

    Initial Commit

---
+++ Ecliptica/src/main/java/com/Spanca05/hero/Application.java
@@ -0,0 +1,9 @@
+package com.Spanca05.hero;
+
+import java.io.IOException;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello World!");
+    }
+}
