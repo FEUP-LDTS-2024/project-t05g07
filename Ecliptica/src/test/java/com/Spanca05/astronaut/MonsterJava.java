a4fb29c229097a55ad5ac9b119fb2e93aabc232e
Vasco Vieira <up202307797@up.pt>
2024-11-25 11:26:27 +0000

    Add Class MonsterTest Class MonsterTest tests Class Monster

---
+++ Ecliptica/src/test/java/com/Spanca05/astronaut/MonsterJava.java
@@ -0,0 +1,40 @@
+package com.Spanca05.astronaut;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MonsterTest {
+    private Monster monster;
+
+    @BeforeEach
+    public void helper() {
+        monster = new Monster(5, 5);
+    }
+
+    @Test
+    void testRandomMove() {
+        Position currentPosition = monster.getPosition();
+        Position newPosition = monster.randomMove(currentPosition);
+
+        int deltaX = Math.abs(newPosition.getX() - currentPosition.getX());
+        int deltaY = Math.abs(newPosition.getY() - currentPosition.getY());
+
+        assertNotEquals(0 , deltaX + deltaY);
+        assertTrue(deltaX <= 1 && deltaY <= 1);
+    }
+
+    @Test
+    void testMove() {
+        Position currentPosition = monster.getPosition();
+        Position newPosition = monster.move(currentPosition);
+
+        int deltaX = Math.abs(newPosition.getX() - currentPosition.getX());
+        int deltaY = Math.abs(newPosition.getY() - currentPosition.getY());
+
+        assertNotEquals(0, deltaX + deltaY);
+        assertTrue(deltaX <= 1 && deltaY <= 1);
+    }
+
+
+}
