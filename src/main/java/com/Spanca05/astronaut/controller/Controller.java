3d6546f2842a0ea8779c6e50f099a806d2691b8e
wings <wings@LAPTOP-0P15TBDV>
2024-11-27 12:20:01 +0000

    Changed Controller slightly (does nothing atm)

--- src/main/java/com/Spanca05/astronaut/controller/Controller.java
+++ src/main/java/com/Spanca05/astronaut/controller/Controller.java
@@ -1,4 +1,20 @@
 package com.Spanca05.astronaut.controller;
 
-public abstract class Controller {
+//import com.Spanca05.astronaut.Game;
+//import com.Spanca05.astronaut.gui.GUI;
+
+import java.io.IOException;
+
+public abstract class Controller<T> {
+    private final T model;
+
+    public Controller(T model) {
+        this.model = model;
+    }
+
+    public T getModel() {
+        return model;
+    }
+
+    //public abstract void step(Game game, GUI.ACTION action, long time) throws IOException;
 }
