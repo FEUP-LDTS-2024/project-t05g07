c651017133f8461527b36ec145b216af7a8b5e77
Francisca <anafranciscapacheco28@gmail.com>
2024-11-29 14:56:24 +0000

    Change movement logic
    The character moves until it founds an obstacle

--- src/main/java/com/Spanca05/astronaut/model/game/elements/Astronaut.java
+++ src/main/java/com/Spanca05/astronaut/model/game/elements/Astronaut.java
@@ -1,18 +1,30 @@
 package com.Spanca05.astronaut.model.game.elements;
 
+import com.Spanca05.astronaut.gui.GUI;
+
 public class Astronaut extends Element {
     private boolean life;
+    private GUI.ACTION direction;
 
     public Astronaut(int x, int y) {
         super(x, y);
         this.life = true;
+        this.direction = null;
+    }
+
+    public void setDirection(GUI.ACTION direction) {
+        this.direction = direction;
+    }
+
+    public GUI.ACTION getDirection() {
+        return direction;
     }
 
     public void die() {
         this.life = false;
     }
 
-    public boolean getLife() {
+    public boolean isAlive() {
         return life;
     }
 }
