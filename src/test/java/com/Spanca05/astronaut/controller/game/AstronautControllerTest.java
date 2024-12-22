2dd5e048e219738b2037735b90dc5d41ffa8d7b1
codenamewings <sofiacarol133@gmail.com>
2024-12-22 04:55:51 +0000

    more AstronautController tests

--- src/test/java/com/Spanca05/astronaut/controller/game/AstronautControllerTest.java
+++ src/test/java/com/Spanca05/astronaut/controller/game/AstronautControllerTest.java
@@ -1,12 +1,24 @@
 package com.Spanca05.astronaut.controller.game;
 
 import com.Spanca05.astronaut.Game;
+import com.Spanca05.astronaut.decorator.Power;
 import com.Spanca05.astronaut.gui.GUI;
 import com.Spanca05.astronaut.model.Position;
 import com.Spanca05.astronaut.model.game.arena.Arena;
 import com.Spanca05.astronaut.model.game.elements.Astronaut;
+import com.Spanca05.astronaut.model.game.elements.Point;
+import com.Spanca05.astronaut.model.game.elements.powerups.BonusCoins;
+import com.Spanca05.astronaut.model.game.elements.powerups.Escudo;
+import com.Spanca05.astronaut.model.game.elements.powerups.Iman;
+import com.Spanca05.astronaut.model.game.elements.powerups.Powerup;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class AstronautControllerTest {
     private Arena helper() {
@@ -170,5 +182,108 @@ public class AstronautControllerTest {
         Mockito.verify(ac, Mockito.times(0)).moveAstronautRight();
         Mockito.verify(ac, Mockito.times(0)).moveAstronautUp();
         Mockito.verify(ac, Mockito.times(0)).moveAstronautDown();
+    }
+
+    @Test
+    void moveAstronautEmpty() throws Exception {
+        Arena arenaMock = Mockito.mock(Arena.class);
+        Position after = new Position(0, 0);
+        Mockito.when(((Power) arenaMock).isEmpty(after)).thenReturn(true);
+        Mockito.when(arenaMock.getCaughtPowerups()).thenReturn(new LinkedList<>());
+
+        GUI.ACTION action = GUI.ACTION.RIGHT;
+        Astronaut astronaut = Mockito.spy(new Astronaut(-1, 0));
+        astronaut.setDirection(action);
+        Mockito.when(arenaMock.getAstronaut()).thenReturn(astronaut);
+
+        AstronautController ac = new AstronautController(arenaMock);
+
+        Game game = Mockito.mock(Game.class);
+        ac.step(game, action, 1000);
+        Mockito.verify(astronaut, Mockito.times(1)).setPosition(after);
+        Mockito.verify(arenaMock, Mockito.times(1)).catchPoint(after);
+        Mockito.verify(astronaut, Mockito.times(0)).setDirection(null);
+    }
+
+    @Test
+    void moveAstronautNotEmpty() throws Exception {
+        Arena arenaMock = Mockito.mock(Arena.class);
+        Position after = new Position(0, 0);
+        Mockito.when(((Power) arenaMock).isEmpty(after)).thenReturn(false);
+        Mockito.when(arenaMock.getCaughtPowerups()).thenReturn(new LinkedList<>());
+
+        GUI.ACTION action = GUI.ACTION.RIGHT;
+        Astronaut astronaut = Mockito.spy(new Astronaut(-1, 0));
+        astronaut.setDirection(action);
+        Mockito.when(arenaMock.getAstronaut()).thenReturn(astronaut);
+
+        AstronautController ac = new AstronautController(arenaMock);
+
+        Game game = Mockito.mock(Game.class);
+        ac.step(game, action, 1000);
+        Mockito.verify(astronaut, Mockito.never()).setPosition(after);
+        Mockito.verify(arenaMock, Mockito.never()).catchPoint(after);
+        Mockito.verify(astronaut, Mockito.times(1)).setDirection(null);
+    }
+
+    @Test
+    public void moveToMonster() throws Exception {
+        Arena arenaMock = Mockito.mock(Arena.class);
+        Position after = new Position(0, 0);
+        Mockito.when(((Power) arenaMock).isEmpty(after)).thenReturn(true);
+        Mockito.when(arenaMock.isMonster(after)).thenReturn(true);
+        Mockito.when(arenaMock.getCaughtPowerups()).thenReturn(new LinkedList<>());
+
+        GUI.ACTION action = GUI.ACTION.RIGHT;
+        Astronaut astronaut = Mockito.spy(new Astronaut(-1, 0));
+        astronaut.setDirection(action);
+        Mockito.when(arenaMock.getAstronaut()).thenReturn(astronaut);
+
+        AstronautController ac = new AstronautController(arenaMock);
+
+        Game game = Mockito.mock(Game.class);
+        ac.step(game, action, 1000);
+        Mockito.verify(astronaut, Mockito.times(1)).setDirection(null);
+        Mockito.verify(astronaut, Mockito.times(1)).die();
+    }
+
+    @Test
+    public void moveToEndBlock() throws Exception {
+        Arena arenaMock = Mockito.mock(Arena.class);
+        Position after = new Position(0, 0);
+        Mockito.when(((Power) arenaMock).isEmpty(after)).thenReturn(true);
+        Mockito.when(arenaMock.isEndBlock(after)).thenReturn(true);
+        Mockito.when(arenaMock.getCaughtPowerups()).thenReturn(new LinkedList<>());
+
+        GUI.ACTION action = GUI.ACTION.RIGHT;
+        Astronaut astronaut = Mockito.spy(new Astronaut(-1, 0));
+        astronaut.setDirection(action);
+        Mockito.when(arenaMock.getAstronaut()).thenReturn(astronaut);
+
+        AstronautController ac = new AstronautController(arenaMock);
+
+        Game game = Mockito.mock(Game.class);
+        ac.step(game, action, 1000);
+        Mockito.verify(astronaut, Mockito.times(1)).win();
+    }
+
+    @Test
+    public void moveToStar() throws Exception {
+        Arena arenaMock = Mockito.mock(Arena.class);
+        Position after = new Position(0, 0);
+        Mockito.when(((Power) arenaMock).isEmpty(after)).thenReturn(true);
+        Mockito.when(arenaMock.isStar(after)).thenReturn(true);
+        Mockito.when(arenaMock.getCaughtPowerups()).thenReturn(new LinkedList<>());
+
+        GUI.ACTION action = GUI.ACTION.RIGHT;
+        Astronaut astronaut = Mockito.spy(new Astronaut(-1, 0));
+        astronaut.setDirection(action);
+        Mockito.when(arenaMock.getAstronaut()).thenReturn(astronaut);
+
+        AstronautController ac = new AstronautController(arenaMock);
+
+        Game game = Mockito.mock(Game.class);
+        ac.step(game, action, 1000);
+        Mockito.verify(arenaMock, Mockito.times(1)).catchStar(after);
     }
 }
